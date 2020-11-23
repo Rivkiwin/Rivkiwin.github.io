343b22ef1c466050ce48b6e63a234856f8923c7d
Rivkiwin <51060145+Rivkiwin@users.noreply.github.com>
2020-11-23 16:39:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const readUrl=require('./readURL');
 const { json } = require('body-parser');
+const PORT = process.env.PORT || 4001
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-app.listen('4001', function () {
+app.listen(PORT, function () {
     console.log("my server is listening to port 4001");
     
 });
