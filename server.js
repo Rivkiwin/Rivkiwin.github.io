be299d35dacb82e4c6627be72cab98fa4f2224d5
Rivkiwin <51060145+Rivkiwin@users.noreply.github.com>
2020-11-23 17:58:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-var port=Number(process.env.PORT || 4001);
+const path = require('path'); 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
+var port=Number(process.env.PORT || 4001);
 app.use(bodyParser.urlencoded({ extended: true }));
 const readUrl=require('./readURL');
 const { json } = require('body-parser');
+app.set("view options", {layout: false});
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
@@ -15,4 +17,8 @@ app.listen(port, function () {
     console.log("my server is listening to port 4001");
     
 });
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
 app.use('/readUrl',readUrl);
+
